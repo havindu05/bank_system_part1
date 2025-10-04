7487a3494d76157b63873b068e7c287253b00d2e
havindu05 <binduhewahavindu2005@gmail.com>
2025-10-04 21:09:46 +0530

    Create UI for welcome page

--- src/main/java/Controller/LoginFormController.java
+++ src/main/java/Controller/LoginFormController.java
@@ -2,10 +2,19 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
+
 
     @FXML
     private TextField txtName;
@@ -13,14 +22,44 @@ public class LoginFormController {
     @FXML
     private TextField txtPassword;
 
-    @FXML
-    void btnForgotPassword(MouseEvent event) {
 
-    }
+
 
     @FXML
     void btnLogin(ActionEvent event) {
+        String name = txtName.getText();
+        String password = txtPassword.getText();
+
+        if (name.equals("Havindu") && password.equals("1234")){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Login Success!");
+            alert.setHeaderText(null);
+            alert.setContentText(" Welcome " + name + " ! ");
+            alert.showAndWait();
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/WelcomePage.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Welcome");
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid username or password!");
+            alert.showAndWait();
+        }
 
     }
 
+    public void btnForgotPassword(MouseEvent mouseEvent) {
+    }
 }
---
+++ src/main/java/Controller/WelcomePageController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomePageController {
+    public void btnNext(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/PinNumber.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Home Page");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
