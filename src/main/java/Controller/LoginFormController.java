b766a375697cae3fd811bc0a82f1a2ebec09e076
havindu05 <binduhewahavindu2005@gmail.com>
2025-10-04 20:06:12 +0530

    Create login form UI

--- src/main/java/Controller/LoginFormController.java
+++ src/main/java/Controller/LoginFormController.java
@@ -1,4 +1,26 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LoginFormController {
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    void btnForgotPassword(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnLogin(ActionEvent event) {
+
+    }
+
 }
