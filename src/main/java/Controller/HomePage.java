cddf1485c227c9ce181a85db632698790647aff2
havindu05 <binduhewahavindu2005@gmail.com>
2025-10-06 16:24:39 +0530

    Create Home Page with logics and UI

--- src/main/java/Controller/HomePage.java
+++ src/main/java/Controller/HomePage.java
@@ -1,4 +1,60 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePage {
+
+    @FXML
+    private TextField txtMoneyShow;
+
+    public void initialize(){
+        txtMoneyShow.setText("Account Balance - Rs:250,000.00");
+        txtMoneyShow.setEditable(false);
+    }
+
+    @FXML
+    void btnAccountDetails(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/HomePage.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Home Page");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void btnDeposite(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTransfacation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnWithdrawal(ActionEvent event) {
+
+    }
+
 }
