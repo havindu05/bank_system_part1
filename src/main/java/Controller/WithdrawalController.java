d5545278df6306e219922021314beac3c5be2c86
havindu05 <binduhewahavindu2005@gmail.com>
2025-10-06 18:20:26 +0530

    Create Withdrawal UI only

--- src/main/java/Controller/WithdrawalController.java
+++ src/main/java/Controller/WithdrawalController.java
@@ -1,4 +1,52 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class WithdrawalController {
+
+    @FXML
+    private TextField txtCashPrice;
+
+    @FXML
+    private TextField txtMoneyShow;
+
+    @FXML
+    void btnCancel(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/HomePage.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Home Page");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showAlert(String msg) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        alert.showAndWait();
+    }
+
+
+    @FXML
+    void btnWithdrawal(ActionEvent event) {
+
+    }
+
 }
